37346dc668d053f4e30d31f83df87e5a5c38f621
WhiteFox <manas.m22@gmail.com>
2018-10-02 11:38:17 +0530

    Update : Made game parameters generalised

--- snake_game1.py
+++ snake_game1.py
@@ -2,27 +2,56 @@ import turtle
 from time import sleep
 from random import randint
 
+# SCREEN CONSTANTS
+scr_width = 600
+scr_height = 600
+init_bg_color = 'green'
+init_title = 'SNAKE GAME'
+
+# SNAKE AND FOOD CONSTANTS
+# colors
+head_color = 'black'
+segment_color = 'blue'
+food_color = 'red'
+# shapes
+head_shape = 'square'
+segment_shape = 'square'
+food_shape = 'circle'
+# initial postions
+head_init_pos = (0, 0)
+food_init_pos = (0, 100)
+
+# UPDATE CONSTANTS
+delay_time = 0.1
+head_move_per_update = 20
+
+# KEYS
+up_key = 'w'
+down_key = 's'
+left_key = 'a'
+right_key = 'd'
+
 # FORMING SCREEN
 window = turtle.Screen()
-window.title("Snake Game")
-window.bgcolor("green")
-window.setup(width=600, height=600)
+window.title(init_title)
+window.bgcolor(init_bg_color)
+window.setup(width=scr_width, height=scr_height)
 window.tracer(0)
 
 # MAKING SNAKE HEAD
 head = turtle.Turtle()
 head.speed(0)
-head.shape("square")
-head.color("black")
+head.shape(head_shape)
+head.color(head_color)
 head.penup()
-head.goto(0, 0)
+head.goto(head_init_pos)
 head.direction = 'stop'
 
 # MAKING FOOD
 food = turtle.Turtle()
-food.shape("circle")
-food.speed(2)
-food.color('red')
+food.shape(food_shape)
+food.speed(0)
+food.color(food_color)
 food.penup()
 food.goto(0, 100)
 
@@ -48,27 +77,24 @@ def on_rt_press():
 
 # LISTENERS FOR KEY PRESS
 window.listen()
-window.onkeypress(on_up_press, 'w')  # When w is pressed, head.direction = up
-window.onkeypress(on_dn_press, 's')  # When s is pressed, head.direction = dn (down)
-window.onkeypress(on_lf_press, 'a')  # When a is pressed, head.direction = lf (left)
-window.onkeypress(on_rt_press, 'd')  # When d is pressed, head.direction = rt (right)
+window.onkeypress(on_up_press, up_key)  # When w is pressed, head.direction = up
+window.onkeypress(on_dn_press, down_key)  # When s is pressed, head.direction = dn (down)
+window.onkeypress(on_lf_press, left_key)  # When a is pressed, head.direction = lf (left)
+window.onkeypress(on_rt_press, right_key)  # When d is pressed, head.direction = rt (right)
 
 # FUNCTION TO MOVE SNAKE
 
 
 def move():
     if head.direction == 'up':
-        head.sety(head.ycor() + 20)
+        head.sety(head.ycor() + head_move_per_update)
     elif head.direction == 'dn':
-        head.sety(head.ycor() - 20)
+        head.sety(head.ycor() - head_move_per_update)
     elif head.direction == 'lf':
-        head.setx(head.xcor() - 20)
+        head.setx(head.xcor() - head_move_per_update)
     elif head.direction == 'rt':
-        head.setx(head.xcor() + 20)
+        head.setx(head.xcor() + head_move_per_update)
 
-
-# DELAY TIME FUNCTION FOR EACH UPDATE
-delay = 0.1
 
 segments = []  # LIST OF SNAKE BODY SEGMENTS
 
@@ -80,11 +106,13 @@ while True:
     if head.distance(food) < 20:
 
         # MOVE FOOD TO RANDOM POSITION
-        food.goto(randint(-290, 290), randint(-290, 290))
+        w = scr_width/2
+        h = scr_height/2
+        food.goto(randint(-w+10, w-10), randint(-h+10, h-10))
 
         # ADD NEW SNAKE BODY SEGMENT
-        new_segment = turtle.Turtle(shape="square")
-        new_segment.color('blue')
+        new_segment = turtle.Turtle(shape=segment_shape)
+        new_segment.color(segment_color)
         new_segment.penup()
         segments.append(new_segment)
 
@@ -94,6 +122,6 @@ while True:
     if len(segments) > 0:
         segments[0].goto(head.xcor(), head.ycor())
     move()
-    sleep(delay)
+    sleep(delay_time)
 
 window.mainloop()
